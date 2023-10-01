453b6f4ac9247a9cf2d16400ad6f93384bf243ed
Storbett21 <96504042+Storbett21@users.noreply.github.com>
2023-10-01 17:41:13 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,42 @@
-import React from "react";
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashCan } from '@fortawesome/free-solid-svg-icons'
+
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [inputValue, setInputValue] = useState("")
+	const [todos, setTodos] = useState([])
+
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center"> <h1 className="titulo">Todos</h1>
+
+			<ul className="list-group list-group-flush">
+				<li className="list-group-item" >
+					<input
+						type="text"
+						onChange={(e) => setInputValue(e.target.value)}
+						value={inputValue}
+						onKeyDown={(e) => {
+							if (e.key === "Enter") {
+								setTodos(todos.concat(inputValue));
+								setInputValue ("");
+							}
+						}}
+						placeholder='Que Necesitas Hacer' />
+				</li>
+				{todos.map((item, index) => (
+					<li className="list-group-item"> {item} <FontAwesomeIcon icon={faTrashCan} onClick={() => setTodos (todos.filter((t, currentIndex) => index != currentIndex))} /></li>
+				))}
+			</ul>
+			<div className='tareas'>{todos.length}</div>
+
 		</div>
 	);
 };
